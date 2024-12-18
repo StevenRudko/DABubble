26c137a824abf66e63185e03e332e586701a4afc
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2024-12-18 17:06:50 +0100

    first commit, add Firebase

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDjKKTUwejTg2eLN49ZWQzxsvz8kSGlhd0",
+        authDomain: "dabubble-796a0.firebaseapp.com",
+        projectId: "dabubble-796a0",
+        storageBucket: "dabubble-796a0.firebasestorage.app",
+        messagingSenderId: "191811841356",
+        appId: "1:191811841356:web:e011d137ce3544df6b73e9"
+    }
+};
