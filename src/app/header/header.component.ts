d6ffb36f70173c533516bca20ee6efad8f5f50fd
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-20 15:54:13 +0100

    general font-family -> Nunito declared

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { LogoComponentComponent } from '../shared/logo-component/logo-component.component';
 import { ProfileOverviewComponent } from '../shared/profile-overview/profile-overview.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponentComponent, MatCardModule, ProfileOverviewComponent],
+  imports: [LogoComponentComponent, MatCardModule, MatCardModule, MatIconModule, ProfileOverviewComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
