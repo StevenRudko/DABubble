bc84c395e439654954ab6d7525ab1f76435fa207
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-20 19:39:53 +0100

    updated header main-chat

--- src/app/main-content/main-chat/main-chat-header/main-chat-header.component.ts
+++ src/app/main-content/main-chat/main-chat-header/main-chat-header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-chat-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './main-chat-header.component.html',
   styleUrl: './main-chat-header.component.scss',
 })
