040a50d30add967ad36dc3ebe0f5d113b8b71462
Steven <steven.rudko@outlook.com>
2024-12-20 21:23:11 +0100

    added sidebar structure

--- src/app/main-content/sidebar/sidebar.component.ts
+++ src/app/main-content/sidebar/sidebar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
   standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {}
