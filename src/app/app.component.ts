5a06a603d9d13fe4ae72977983035d67e3d793ae
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-20 20:17:44 +0100

    updated sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './main-content/sidebar/sidebar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
