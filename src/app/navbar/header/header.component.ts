05069846695ac439f9c306d5d2c00dbf6a2e5cdd
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-20 14:45:16 +0100

    shared c logo-component added

--- src/app/navbar/header/header.component.ts
+++ src/app/navbar/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ProfileOverviewComponent } from './profile-overview/profile-overview.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { LogoComponentComponent } from '../../shared/logo-component/logo-component.component';
 
 @Component({
   selector: 'app-header',
-  standalone:true,
-  imports: [ProfileOverviewComponent, MatCardModule ],
+  standalone: true,
+  imports: [ProfileOverviewComponent, LogoComponentComponent, MatCardModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-
 export class HeaderComponent {}
---
+++ src/app/shared/logo-component/logo-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo-component.component.html',
+  styleUrl: './logo-component.component.scss',
+})
+export class LogoComponentComponent {}
