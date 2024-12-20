b207f7c2efd8e72fdf7eb42fc67f02db14d0e24f
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-20 17:57:21 +0100

    new components for main-content

---
+++ src/app/main-content/main-chat/main-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-chat',
+  imports: [],
+  templateUrl: './main-chat.component.html',
+  styleUrl: './main-chat.component.scss'
+})
+export class MainChatComponent {
+
+}
