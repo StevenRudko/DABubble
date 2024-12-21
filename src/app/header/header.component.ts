920f14b2d3384a93fea9a11d37e469ec47f8535b
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-21 21:06:29 +0100

    edited header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { LogoComponentComponent } from '../shared/logo-component/logo-component.component';
-import { ProfileOverviewComponent } from '../shared/profile-overview/profile-overview.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ProfileOverviewComponent } from './profile-overview/profile-overview.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponentComponent, MatCardModule, MatCardModule, MatIconModule, ProfileOverviewComponent],
+  imports: [
+    LogoComponentComponent,
+    MatCardModule,
+    MatIconModule,
+    MatDialogModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog(): void {
+    // console.log('The dialog is open');
+    const dialogRef = this.dialog.open(ProfileOverviewComponent, {
+
+      // data: {name: this.name(), animal: this.animal()},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      // if (result !== undefined) {
+      //   this.animal.set(result);
+      // }
+    });
+  }
+
+}
