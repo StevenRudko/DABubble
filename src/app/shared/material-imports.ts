39ab2ab09b4ef1782f8a809270bb87dbc1680a54
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-21 21:18:09 +0100

    material-imports edited

--- src/app/shared/material-imports.ts
+++ src/app/shared/material-imports.ts
@@ -3,6 +3,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 export const MATERIAL_MODULES = [
     MatCardModule,
