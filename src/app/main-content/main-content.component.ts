fd7a7bd04714cdf5a0888c12658ad4bf95ece0e4
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-21 23:39:50 +0100

    edited sidebar btn

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { ThreadComponent } from './thread/thread.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-// import { SidebarComponent } from "./sidebar/sidebar.component";
+import { MATERIAL_MODULES } from '../shared/material-imports';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [SidebarComponent, MainChatComponent, ThreadComponent ],
+  imports: [
+    SidebarComponent,
+    MainChatComponent,
+    ThreadComponent,
+    NgIf,
+    CommonModule,
+    MATERIAL_MODULES,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
 export class MainContentComponent {
+  sidebarActive:boolean = false;
 
+  openSidebar() {
+    this.sidebarActive = !this.sidebarActive;
+  }
 }
