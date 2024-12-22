83af455b3226a1f7edc3a13ac1c192ce4ce46896
Steven <steven.rudko@outlook.com>
2024-12-22 16:05:27 +0100

    added material sidenav + emoji-hover in user-message

--- src/app/shared/user-message/user-message.component.ts
+++ src/app/shared/user-message/user-message.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HoverService } from '../../../assets/service/hover.service';
+import { MessageEmojisComponent } from '../message-emojis/message-emojis.component';
 
 @Component({
   selector: 'app-user-message',
   standalone: true,
-  imports: [],
+  imports: [MessageEmojisComponent],
   templateUrl: './user-message.component.html',
   styleUrl: './user-message.component.scss',
 })
