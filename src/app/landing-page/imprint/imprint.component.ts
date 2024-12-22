02a74b91cfbed062c7f10a33498e6e81607446b2
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2024-12-23 00:54:36 +0100

    add imprint and start layout and style, add privacy-policy component

---
+++ src/app/landing-page/imprint/imprint.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../../shared/material-imports';
+
+@Component({
+  selector: 'app-imprint',
+  imports: [MATERIAL_MODULES],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+
+}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,10 +4,18 @@ import { MATERIAL_MODULES } from '../shared/material-imports';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { CreateAccountComponent } from "./create-account/create-account.component";
 import { AvatarPickerComponent } from './avatar-picker/avatar-picker.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LogoComponentComponent, MATERIAL_MODULES, UserLoginComponent, CreateAccountComponent, AvatarPickerComponent],
+  imports: [LogoComponentComponent,
+    MATERIAL_MODULES,
+    UserLoginComponent,
+    CreateAccountComponent,
+    AvatarPickerComponent,
+    PrivacyPolicyComponent,
+    ImprintComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
