f7ad50c75dcc93750617285bc6f292d40a6df273
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2024-12-23 00:38:32 +0100

    add avatar-picker

---
+++ src/app/landing-page/avatar-picker/avatar-picker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../../shared/material-imports';
+
+@Component({
+  selector: 'app-avatar-picker',
+  imports: [MATERIAL_MODULES],
+  templateUrl: './avatar-picker.component.html',
+  styleUrl: './avatar-picker.component.scss'
+})
+export class AvatarPickerComponent {
+
+  avatarPath: string[] = ['img-placeholder/elias.svg', 'img-placeholder/elise.svg', 'img-placeholder/frederik.svg', 'img-placeholder/noah.svg', 'img-placeholder/sofia.svg', 'img-placeholder/steffen.svg',]
+}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,10 +3,11 @@ import { LogoComponentComponent } from '../shared/logo-component/logo-component.
 import { MATERIAL_MODULES } from '../shared/material-imports';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { CreateAccountComponent } from "./create-account/create-account.component";
+import { AvatarPickerComponent } from './avatar-picker/avatar-picker.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LogoComponentComponent, MATERIAL_MODULES, UserLoginComponent, CreateAccountComponent],
+  imports: [LogoComponentComponent, MATERIAL_MODULES, UserLoginComponent, CreateAccountComponent, AvatarPickerComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
