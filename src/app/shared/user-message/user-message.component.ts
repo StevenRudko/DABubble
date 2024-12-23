4aeb8a88a7dbf54050e436ac0a517dbe1e6f845e
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-23 16:30:54 +0100

    emojis box edited

--- src/app/shared/user-message/user-message.component.ts
+++ src/app/shared/user-message/user-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MessageEmojisComponent } from '../message-emojis/message-emojis.component';
 import { NgIf } from '@angular/common';
 import { MATERIAL_MODULES } from '../material-imports';
