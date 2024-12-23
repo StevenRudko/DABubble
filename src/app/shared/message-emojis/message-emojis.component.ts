bb6a5565d114a494b58ba8b7485ea02cb0c1ac47
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-23 19:18:26 +0100

    fixed bug

--- src/app/shared/message-emojis/message-emojis.component.ts
+++ src/app/shared/message-emojis/message-emojis.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { MATERIAL_MODULES } from '../material-imports';
 import { InputOutput } from '../../service/input-output.service';
-import { Observable } from 'rxjs';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-message-emojis',
   standalone: true,
-  imports: [MATERIAL_MODULES, NgIf],
+  imports: [MATERIAL_MODULES],
   templateUrl: './message-emojis.component.html',
   styleUrls: ['./message-emojis.component.scss'],
 })
