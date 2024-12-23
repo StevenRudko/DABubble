bac505c6f41fe2a65707a35c91163194e221a82f
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-23 15:52:51 +0100

    edited main-chat

--- src/app/main-content/main-chat/main-chat.component.ts
+++ src/app/main-content/main-chat/main-chat.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { MainChatHeaderComponent } from './main-chat-header/main-chat-header.component';
 import { MessageInputBoxComponent } from '../../shared/message-input-box/message-input-box.component';
 import { UserMessageComponent } from '../../shared/user-message/user-message.component';
+import { MainChatDailyMessagesComponent } from '../main-chat-daily-messages/main-chat-daily-messages.component';
 
 @Component({
   selector: 'app-main-chat',
   standalone: true,
-  imports: [MainChatHeaderComponent, UserMessageComponent, MessageInputBoxComponent],
+  imports: [MainChatHeaderComponent, MainChatDailyMessagesComponent, MessageInputBoxComponent],
   templateUrl: './main-chat.component.html',
   styleUrl: './main-chat.component.scss'
 })
+
 export class MainChatComponent {
 
 }
