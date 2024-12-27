c9024861f51e0775d875f962e749f8bd9e01e5e0
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-27 23:39:20 +0100

    edited profile overview

--- src/app/header/profile-overview/profile-overview.component.ts
+++ src/app/header/profile-overview/profile-overview.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../../shared/material-imports';
 
 @Component({
   selector: 'app-profile-overview',
   standalone: true,
-  imports: [],
+  imports: [MATERIAL_MODULES],
   templateUrl: './profile-overview.component.html',
   styleUrl: './profile-overview.component.scss',
 })
