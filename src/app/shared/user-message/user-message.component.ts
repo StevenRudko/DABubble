f056c4efa4ffee2c2a2eed9d85a66307828d503f
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-27 17:26:42 +0100

    edited user message

--- src/app/shared/user-message/user-message.component.ts
+++ src/app/shared/user-message/user-message.component.ts
@@ -23,12 +23,6 @@ export class UserMessageComponent {
   isThreadContext: boolean = false;
   @Output() openThreadEvent = new EventEmitter<void>();
 
-  private inputOutputService = inject(InputOutput);
-
-  ngOnInit() {
-    this.isThreadContext = this.isThread;
-  }
-
   onMouseEnter() {
     this.isHoveredActive = true;
     this.showReactionEmojis = true;
