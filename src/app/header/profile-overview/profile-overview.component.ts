d2b9d3863a54ef2de18300454d84951fbbee01e4
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2024-12-30 00:17:22 +0100

    add dialogRef.close() and imports to profile-overview.component

--- src/app/header/profile-overview/profile-overview.component.ts
+++ src/app/header/profile-overview/profile-overview.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MATERIAL_MODULES } from '../../shared/material-imports';
+import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { HeaderComponent } from '../header.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile-overview',
@@ -8,4 +12,14 @@ import { MATERIAL_MODULES } from '../../shared/material-imports';
   templateUrl: './profile-overview.component.html',
   styleUrl: './profile-overview.component.scss',
 })
-export class ProfileOverviewComponent {}
+export class ProfileOverviewComponent {
+  readonly dialogRef = inject(MatDialogRef<HeaderComponent>);
+  authService = inject(AuthService)
+  router = inject(Router);
+
+  logOut(): void {
+    this.authService.logout();
+    this.dialogRef.close();
+    this.router.navigateByUrl('');
+  }
+}
