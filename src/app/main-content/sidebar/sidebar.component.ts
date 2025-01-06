dcc21691632dfc9df379a4e549a3c6227001e514
Steven <steven.rudko@outlook.com>
2025-01-06 14:31:19 +0100

    Database for channels connected in sidebar

--- src/app/main-content/sidebar/sidebar.component.ts
+++ src/app/main-content/sidebar/sidebar.component.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MATERIAL_MODULES } from '../../shared/material-imports';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { ChannelDialogComponent } from './create-channel-dialog/create-channel-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Channel {
+  id: string;
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, MATERIAL_MODULES],
+  imports: [CommonModule, MatDialogModule, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-  constructor(private dialog: MatDialog) {}
+export class SidebarComponent implements OnInit {
+  channels$: Observable<Channel[]>;
+  isChannelSectionExpanded: boolean = true;
+  isDirectMessageSectionExpanded: boolean = true;
 
-  showDialog = false;
+  constructor(private dialog: MatDialog, private firestore: Firestore) {
+    const channelsCollection = collection(this.firestore, 'channels');
+    this.channels$ = collectionData(channelsCollection, {
+      idField: 'id',
+    }) as Observable<Channel[]>;
+  }
+
+  ngOnInit() {
+    // Nicht mehr benötigt, da wir jetzt die komplette Collection beobachten
+  }
 
   openChannelDialog() {
     const dialogRef = this.dialog.open(ChannelDialogComponent);
+  }
+
+  toggleChannelSection() {
+    this.isChannelSectionExpanded = !this.isChannelSectionExpanded;
+  }
+
+  toggleDirectMessageSection() {
+    this.isDirectMessageSectionExpanded = !this.isDirectMessageSectionExpanded;
   }
 }
