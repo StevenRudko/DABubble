e158fb1ce7c2ab26ea9b305978c87bbccc266b36
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2025-01-15 00:46:06 +0100

    add emoji dialog

---
+++ src/app/shared/emoji-picker/emoji-picker.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emoji-picker',
+  standalone: true,
+  imports: [CommonModule, NgFor, NgIf],
+  templateUrl: './emoji-picker.component.html',
+  styleUrls: ['./emoji-picker.component.scss'],
+})
+export class EmojiPickerComponent implements OnInit {
+  emojiList: any[] = [
+    { name: 'smile', emoji: '😊' },
+    { name: 'thumbs_up', emoji: '👍' },
+    { name: 'heart', emoji: '❤️' },
+    { name: 'rocket', emoji: '🚀' },
+    { name: 'laughing', emoji: '😂' },
+    { name: 'clap', emoji: '👏' },
+    { name: 'fire', emoji: '🔥' },
+    { name: 'star', emoji: '⭐' },
+    { name: 'sunglasses', emoji: '😎' },
+    { name: 'thinking', emoji: '🤔' },
+    { name: 'wink', emoji: '😉' },
+    { name: 'crying', emoji: '😭' },
+    { name: 'kiss', emoji: '😘' },
+    { name: 'celebrate', emoji: '🎉' },
+    { name: 'party', emoji: '🥳' },
+    { name: 'confetti', emoji: '🎊' },
+    { name: 'thumbs_down', emoji: '👎' },
+    { name: 'ok_hand', emoji: '👌' },
+    { name: 'prayer', emoji: '🙏' },
+    { name: 'praise', emoji: '🙌' },
+    { name: 'check', emoji: '✅' },
+  ];
+
+  selectedEmojis: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Callback, das aufgerufen wird, wenn ein Emoji ausgewählt wird
+  onEmojiSelect(emoji: any): void {
+    this.selectedEmojis.push(emoji.emoji);
+    console.log('Ausgewähltes Emoji:', emoji.emoji);
+  }
+}
--- src/app/shared/user-message/user-message.component.ts
+++ src/app/shared/user-message/user-message.component.ts
@@ -2,11 +2,13 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
 import { MATERIAL_MODULES } from '../material-imports';
 import { InputOutput } from '../../service/input-output.service';
+import { EmojiPickerComponent } from '../emoji-picker/emoji-picker.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-message',
   standalone: true,
-  imports: [CommonModule, MATERIAL_MODULES, NgIf],
+  imports: [CommonModule, MATERIAL_MODULES, NgIf, EmojiPickerComponent],
   templateUrl: './user-message.component.html',
   styleUrl: './user-message.component.scss',
 })
@@ -19,24 +21,27 @@ export class UserMessageComponent {
   @Input() messagesToday: { timestamp: number; userMessageId: string; author: string, message: string; emojis: string[]; hours: number; minutes: number }[] = [];
   @Input() messagesOld: { timestamp: number; userMessageId: string; author: string; message: string; emojis: string[]; hours: number; minutes: number }[] = [];
 
-  isHoveredActive: boolean = false;
-  isThreadContext: boolean = false;
+  hoverStateMap: { [userMessageId: string]: boolean } = {};
   @Output() openThreadEvent = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   openThread() {
     this.openThreadEvent.emit();
   }
 
-  onMouseEnter() {
-    this.isHoveredActive = true;
-    this.showReactionEmojis = true;
+  onMouseEnter(msgId: string) {
+    this.hoverStateMap[msgId] = true;
   }
 
-  onMouseLeave() {
-    this.isHoveredActive = false;
-    this.showReactionEmojis = false;
+  onMouseLeave(msgId: string) {
+    this.hoverStateMap[msgId] = false;
   }
 
+  openEmojiPicker(): void {
+    const dialogRef = this.dialog.open(EmojiPickerComponent, {});
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
