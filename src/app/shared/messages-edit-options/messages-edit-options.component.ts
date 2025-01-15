9382b37fcd3d1130f985336747152c2ae8eae186
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2025-01-15 22:14:59 +0100

    updated edit options

--- src/app/shared/messages-edit-options/messages-edit-options.component.ts
+++ src/app/shared/messages-edit-options/messages-edit-options.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../material-imports';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-messages-edit-options',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MATERIAL_MODULES],
   templateUrl: './messages-edit-options.component.html',
   styleUrl: './messages-edit-options.component.scss'
 })
