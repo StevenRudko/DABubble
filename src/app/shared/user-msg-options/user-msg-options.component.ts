c0123071a4941382b79098ed38c94e77d2aa11ec
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2025-01-19 18:24:40 +0100

    updated user msg

--- src/app/shared/user-msg-options/user-msg-options.component.ts
+++ src/app/shared/user-msg-options/user-msg-options.component.ts
@@ -3,27 +3,35 @@ import { MATERIAL_MODULES } from '../material-imports';
 import { CommonModule, NgIf } from '@angular/common';
 import { EmojiPickerComponent } from '../emoji-picker/emoji-picker.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MessagesEditOptionsComponent } from '../messages-edit-options/messages-edit-options.component';
 
 @Component({
   selector: 'app-user-msg-options',
   standalone: true,
-  imports: [CommonModule, MATERIAL_MODULES, EmojiPickerComponent],
+  imports: [CommonModule, MATERIAL_MODULES, EmojiPickerComponent, MessagesEditOptionsComponent],
   templateUrl: './user-msg-options.component.html',
   styleUrl: './user-msg-options.component.scss'
 })
 
-
-
 export class UserMsgOptionsComponent {
   hoverFaceTag: boolean = false;
+  hoverEdit: boolean = false;
 
   constructor() {}
 
-  onMouseEnter() {
-    this.hoverFaceTag = true;
+  onMouseEnter(obj: string) {
+    if(obj === 'tag_face') {
+      this.hoverFaceTag = true;
+    } else if (obj === 'edit') {
+      this.hoverEdit = true;
+    }
   }
 
-  onMouseLeave() {
-    this.hoverFaceTag = false;
+  onMouseLeave(obj: string) {
+    if(obj === 'tag_face') {
+      this.hoverFaceTag = false;
+    } else if (obj === 'edit') {
+      this.hoverEdit = false;
+    }
   }
 }
