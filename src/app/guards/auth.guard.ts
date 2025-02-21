ccce9ea5a9ebf887d7036362e8c26f62af8d02ad
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2025-02-21 22:17:12 +0100

    update guard auth

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,39 +9,19 @@ import { map } from 'rxjs';
  * - If no user is authenticated, the guard redirects to the homepage (``).
  * @returns {Observable<boolean>} - An observable that resolves to `true` (access granted) or `false` (access denied).
  */
-
-// soll verhindern, dass beim reload (direkteingabe der url) ein error bildschirm kommt
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
 
   return authService.user$.pipe(
     map((user) => {
       if (user) {
         return true;
       } else {
-        setTimeout(() => router.navigate(['']), 0); // Asynchrones Redirect
+        router.navigate(['']);
         return false;
       }
     })
   );
 };
-
-// alter stand
-
-// export const authGuard: CanActivateFn = () => {
-//   const authService = inject(AuthService);
-//   const router = inject(Router);
-
-
-//   return authService.user$.pipe(
-//     map((user) => {
-//       if (user) {
-//         return true;
-//       } else {
-//         router.navigate(['']);
-//         return false;
-//       }
-//     })
-//   );
-// };
