f0ae6709f7f8a78196a5139bc5f866116c86161e
Steven <steven.rudko@outlook.com>
2025-03-11 12:03:28 +0100

    Routing Update: Switched from Hash to Path Location Strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router'; // withHashLocation hinzufügen
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
@@ -13,7 +13,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()), // withHashLocation hinzugefügt
+    provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
